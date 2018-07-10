fc68034354d5132694a04a20ce90b4eb30999739
chrismorrobel <cm288@njit.edu>
2018-07-09 22:56:05 -0400

    Commit #16: Created a unit test to count the number of records inserted by the UserTableSeeder.

---
+++ tests/Unit/UserCountTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserCountTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUserCount()
+    {
+        $users = User::All();
+        $userCount = 50;
+        $recordCount = $users->count();
+
+        $this->assertEquals($userCount, $recordCount);
+    }
+}
