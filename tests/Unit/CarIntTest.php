1d6e4c03b7c06601a07fbb5076b6997e76ded627
chrismorrobel <cm288@njit.edu>
2018-07-10 19:58:03 -0400

    Commit #21: Created a unit test to check if the year field in the Car table is of type "int"

---
+++ tests/Unit/CarIntTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Constraint\IsType;
+use App\Car;
+
+class CarIntTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testYearType()
+    {
+        $car = new Car();
+        $year = (int)$car->year;
+
+        //echo $year;
+
+        $this->assertInternalType(IsType::TYPE_INT, $year);
+    }
+}
