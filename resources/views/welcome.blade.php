68a259cf2a872bc33810060e2fd2427be1bf5158
chrismorrobel <cm288@njit.edu>
2018-07-09 21:28:03 -0400

    Commit #9: Created a feature test to check if the register page returns a 200 status code.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,9 +85,6 @@
                 <div class="links">
                     <a href="{{route('about')}}">About</a>
                     <a href="{{route('contact')}}">Contact Me</a>
-                    <!-- <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
                 </div>
             </div>
         </div>
